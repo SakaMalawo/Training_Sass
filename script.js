e963f363a7c1f7ba75c3ae0d94a2554f81159bdf
Saka Malawo <sakamalawo@gmail.com>
2025-01-29 20:53:47 +0300

    Load content

--- script.js
+++ script.js
@@ -39,4 +39,26 @@ document.addEventListener('DOMContentLoaded', function() {
       goToItem(currentItem);
     });
   });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    let focusSections = document.querySelectorAll("#focusOne p, #focusTwo p");
+
+    function checkVisibility() {
+        focusSections.forEach(section => {
+            let rect = section.getBoundingClientRect();
+            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+                section.style.opacity = 1;
+                section.style.transform = "translateX(0)";
+                section.style.transition = "transform 2s ease-in-out, opacity 2s ease-in-out";
+            } else {
+                section.style.opacity = 0;
+                section.style.transform = "translateX(-100%)";
+            }
+        });
+    }
+
+    window.addEventListener("scroll", checkVisibility);
+    checkVisibility();
+});
+
   
